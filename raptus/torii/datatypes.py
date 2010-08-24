d15471271799374367e43d30291385ca3d69b638
Raptus <dev@raptus.com>
2010-08-24 23:27:36 +0000

    catch stderr over the class AutoFormattedTB, fix open database connection, reset stdout each command

--- raptus/torii/datatypes.py
+++ raptus/torii/datatypes.py
@@ -8,18 +8,14 @@ from ZServer import CONNECTION_LIMIT, requestCloseOnExec
 import asyncore
 import Zope2
 from IPython.ipmaker import make_IPython
-from IPython.Prompts import CachedOutput
+from IPython.ultraTB import AutoFormattedTB as BaseAutoFormattedTB
 from codeop import compile_command
 from pprint import PrettyPrinter
 import traceback
 from raptus.torii import config,utility
 from raptus.torii import carrier
 
-
 def result_display(self, arg):
-    from IPython.hooks import result_display
-    result_display(self, arg)
-    import StringIO
     self.outStringIO = StringIO.StringIO()
     if self.rc.pprint:
         out =  PrettyPrinter().pformat(arg)
@@ -29,6 +25,13 @@ def result_display(self, arg):
     else:
         print >> self.outStringIO, repr(arg)
     return None 
+
+class AutoFormattedTB(BaseAutoFormattedTB):
+    def __call__(self, *args, **kw):
+        self.errStringIO = StringIO.StringIO()
+        kw.update(dict(out=self.errStringIO))
+        return BaseAutoFormattedTB.__call__(self, *args, **kw)
+
 
 class ToriiServer(asyncore.dispatcher):
     def __init__(self, path, logger):
@@ -57,41 +60,41 @@ class ToriiServer(asyncore.dispatcher):
                 
         self.interpreter = make_IPython(argv=[],embedded=True,user_global_ns=self.locals)
         self.interpreter.set_hook('result_display',result_display)
-        self.interpreter.InteractiveTB.out = StringIO.StringIO()
-        """
-        cache = CachedOutput(interpreter,
-                             interpreter.rc.cache_size,
-                             interpreter.rc.pprint,
-                             input_sep = interpreter.rc.separate_in,
-                             output_sep = interpreter.rc.separate_out,
-                             output_sep2 = interpreter.rc.separate_out2,
-                             ps1 = interpreter.rc.prompt_in1,
-                             ps2 = interpreter.rc.prompt_in2,
-                             ps_out = interpreter.rc.prompt_out,)
-        """
+        color = self.interpreter.InteractiveTB.old_scheme
+        self.interpreter.InteractiveTB = AutoFormattedTB(mode = 'Plain',
+                                                         color_scheme=color,
+                                                         tb_offset = 1)
+
         try:
             db, aname, version_support = Zope2.bobo_application._stuff
             connection = db.open()
-
             app=Zope2.bobo_application(connection)
             self.locals.update(dict(app=app))
             options = self.conversation(conn, carrier.FetchOptions())
             self.locals.update(parser=options.parser)
-            #self.conversiontion(conn, carrier.SendDisplayHook(interpreter.readline))
+
             while True:
+                """ reset stdout and stderr stream each time
+                """
+                self.interpreter.outStringIO = StringIO.StringIO()
+                self.interpreter.InteractiveTB.errStringIO = StringIO.StringIO()
+                
                 input = self.conversation(conn, carrier.GetCodeLine(self.interpreter))
                 try:
                     while self.interpreter.push(input.line):
                         input = self.conversation(conn, carrier.GetNextCodeLine(self.interpreter))
                 except Exception, mesg:
-                    self.conversation(conn, carrier.SendStderr(genutils.Term.cout.stream))
-                self.conversation(conn, carrier.SendStdout(self.interpreter.outStringIO))
+                    pass
+                stderr = self.interpreter.InteractiveTB.errStringIO
+                if stderr.len:
+                    self.conversation(conn, carrier.SendStderr(stderr))
+                stdout = self.interpreter.outStringIO
+                if stdout.len:
+                    self.conversation(conn, carrier.SendStdout(stdout))
                 
-            connection.close()
-            del sys.stdout
-            conn.shutdown(1)
             
         except Exception,meg:
+            connection.close()
             conn.close()
 
     def conversation(self, connection, carrierObject):
@@ -116,24 +119,6 @@ class ToriiServer(asyncore.dispatcher):
         asyncore.dispatcher.create_socket(self, family, type)
         #requestCloseOnExec(self.socket)
 
-
-    def showtraceback(self):
-        try:
-            type, value, tb = sys.exc_info()
-            sys.last_type = type
-            sys.last_value = value
-            sys.last_traceback = tb
-            tblist = traceback.extract_tb(tb)
-            del tblist[:1]
-            list = traceback.format_list(tblist)
-            if list:
-                list.insert(0, "Traceback (most recent call last):\n")
-            list[len(list):] = traceback.format_exception_only(type, value)
-        finally:
-            tblist = tb = None
-            err = StringIO.StringIO()
-            err.writelines(list)
-            return err
 
 class ToriiFactory(ServerFactory):
     
